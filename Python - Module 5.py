bb57ec45fc50db4825ee526c60863cbcf19a509f
Runzhou Zhu <runzhouz@metropolia.fi>
2023-09-09 18:23:42 +0300

    123

--- Python - Module 5.py
+++ Python - Module 5.py
@@ -1,8 +1,48 @@
-lst=[]
+'''
+#Question 1
+import random
+
+
+diceRoll = []
+n = int(input("Please enter the number of dices :"))
+i = 0
+sum = 0
+while i < n:
+    diceRoll.append(random.randint(1,6))
+    i = i + 1
+    
+for x in diceRoll:
+    sum = sum + x
+    
+print(sum)
+
+########################################
+#Question 2
+numbers = []
 while True:
-    a = input("")
-    if a != '':
-        lst = lst.append(a)
+    n = input("Please enter a number: ")
+    if n == '':
+        break
+    numbers.append(n)
+    
+i = 0
+numbers.sort(reverse=True)
+while i < 5:
+    print(numbers[i])
+    i = i + 1
+'''
+########################################
+#Question 3
+integer = int(input("Please enter a interger: "))
+i = 1
+while i < integer:
+    i = i + 1
+    if integer % i == 0:
+        print("This is not a prime number.")
+        break
     else:
-        for x in lst:
-            print(x)
+        print("This is a prime number.")
+        break
+    
+########################################
+#Question 4
--- Python - Module 6.py
+++ Python - Module 6.py
@@ -1,5 +1,10 @@
-def greet():
-    print("Hello!")
-    return
+#sum substract multiply division 
+#sum
+def sum(a,b,c):
+    sum = a + b + c
+    return sum
 
-greet()
+def sub(a,b,c):
+    sub = a - b - c
+    return sub
+
