dd652d95245bc72c0fd248fd409ebcab98c93acd
Runzhou Zhu <runzhouz@metropolia.fi>
2023-09-15 12:11:29 +0300

    123

--- Python - Module 8.py
+++ Python - Module 8.py
@@ -1,12 +1,56 @@
+'''
+#Question 1
 import mysql.connector
 
 connection = mysql.connector.connect(
     host = '127.0.0.1',
     port = 3306,
-    database = 'duckburg',
+    database = 'flight_game',
     user = 'root',
     password = 'hhdhhdhhd',
     autocommit = True
     )
 
+def getAirportAndTown(ICAO):
+    sql="select name, municipality from airport"
+    sql+=" where ident = '" + ICAO + "'"
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            print(f"ICAO:{ICAO}\nAirport name:{row[0]}\nLocation:{row[1]}")
+    return
 
+ICAO = input("Please enter the ICAO code: ")
+getAirportAndTown(ICAO)
+'''
+########################################
+#Question 2
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host = '127.0.0.1',
+    port = '3306',
+    database = 'flight_game',
+    user = 'root',
+    password = 'hhdhhdhhd',
+    autocommit = True
+    )
+
+def getAirportType(areaCode):
+    sql = "select name, type from airport"
+    sql+= " where iso_country = '" + areaCode + "'"
+    sql+= " order by type" 
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            print(f"{row[0]}\t\t\t\t{row[1]}")
+    return
+
+areaCode = input("Please enter the area code: ")
+getAirportType(areaCode)
