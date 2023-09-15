0f4ed478dac5b7d045b2502b9bec993749817be7
Runzhou Zhu <runzhouz@metropolia.fi>
2023-09-15 09:43:25 +0300

    123

--- Python - Module 7.py
+++ Python - Module 7.py
@@ -14,24 +14,68 @@ def find_season(month):
 
 season = int(input("Please enter the number of month: "))
 print(f"The season is {find_season(season)}")
-'''
+
 ########################################
 #Question 2
 def add_name(name,names):
-    a = names
-    names.add(name)
-    if a == names:
+    if name in names:
         print("Existing name")
     else:
+        names.add(name)
         print("New name")
     
 names = set()
 while True:
     i = input("Please enter a name: ")
     if i == '':
+        for x in names:
+            print(x)
         break
     else:
         add_name(i,names)
-        
+'''    
 ########################################
 #Question 3
+def new_airport(ICAO,name,airports):
+    airports[ICAO] = name
+    
+def fetch_airport(ICAO,airports):
+    name = airports[ICAO]
+    return name
+
+airports = {}
+while True:
+    print("\nOptions:")
+    print("Enter 1 to enter a new airport.")
+    print("Enter 2 to fetch airport information.")
+    print("Enter any other thing to quit.")
+    i = input("Please enter your choice: ")
+    
+    if i == '1':
+        print("\nNew airport: ")
+        ICAO = input("Please enter the ICAO code: ")
+        name = input("Please enter the name: ")
+        if ICAO in airports:
+            print("ICAO already exists.")
+        elif name in airports:
+            print("Name already exists.")
+        else:
+            new_airport(ICAO,name,airports)
+            print("Airport added.")
+        
+    elif i == '2':
+        print("\nFetch information: ")
+        ICAO = input("Please enter the ICAO code: ")
+        print(f"The airport is {fetch_airport(ICAO,airports)}.")
+        
+    else:
+        print("END")
+        break
+            
+            
+        
+
+    
+
+
+
