f3c6c842580008fd24dbe438870157877ba969f7
Runzhou Zhu <runzhouz@metropolia.fi>
2023-09-18 11:25:00 +0300

    123

--- Python - Module 8.py
+++ Python - Module 8.py
@@ -25,7 +25,7 @@ def getAirportAndTown(ICAO):
 
 ICAO = input("Please enter the ICAO code: ")
 getAirportAndTown(ICAO)
-'''
+
 ########################################
 #Question 2
 import mysql.connector
@@ -54,3 +54,38 @@ def getAirportType(areaCode):
 
 areaCode = input("Please enter the area code: ")
 getAirportType(areaCode)
+'''
+########################################
+# Question 3
+from geopy import distance
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host = '127.0.0.1',
+    port = '3306',
+    database = 'flight_game',
+    user = 'root',
+    password = 'hhdhhdhhd',
+    autocommit = True
+    )
+
+def getDistance(ICAO1, ICAO2):
+    sql1 = f"select latitude_deg, longitude_deg from airport where ident = '{ICAO1}'"
+    cursor1 = connection.cursor()
+    cursor1.execute(sql1)
+    result1 = cursor1.fetchall()
+    sql2 = f"select latitude_deg, longitude_deg from airport where ident = '{ICAO2}'"
+    cursor2 = connection.cursor()
+    cursor2.execute(sql2)
+    result2 = cursor2.fetchall()
+
+    return distance.distance(result1,result2)
+
+
+print(getDistance('00A','00AA'))
+
+    
+
+    
+
+
