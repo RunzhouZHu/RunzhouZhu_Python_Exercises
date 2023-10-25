2fb302ecd4c6332b15a6c31ad8301624da5817b7
Runzhou Zhu <runzhouz@metropolia.fi>
2023-10-25 10:23:40 +0300

    123

--- Python - Module 11.py
+++ Python - Module 11.py
@@ -1,1 +1,72 @@
+﻿'''
+#Question 1
+class Publication:
+    def __init__(self, name):
+        self.name = name
+        
+class Book(Publication):
+    def __init__(self, name, author, pageCount):
+        super().__init__(name)
+        self.author = author
+        self.pageCount = pageCount
+        
+    def print_information(self):
+        print(f"The book name is {self.name}, the author is {self.author}, it has {self.pageCount} pages.")
+        
+class Magazine(Publication):
+    def __init__(self, name, chiefEditor):
+        super().__init__(name)
+        self.chiefEditor = chiefEditor
+        
+    def print_information(self):
+        print(f"The magazine name is {self.name}, the chief editor is {self.chiefEditor}.")
+        
+magazine = Magazine('Donald Duck', 'Aki Hyyppä')
+book = Book('Compartment No. 6', 'Rosa Liksom', 192)
 
+magazine.print_information()
+book.print_information()
+'''
+########################################
+#Question 2
+import random
+
+
+class Car:
+    def __init__(self, registrationNumber, maximumSpeed):
+        self.registrationNumber = registrationNumber
+        self.maximumSpeed = maximumSpeed
+        self.currentSpeed = 0
+        self.travelledDistance = 0
+        
+    def accelerate(self, speed):
+        self.currentSpeed = self.currentSpeed + speed
+        if self.currentSpeed < 0:
+            self.currentSpeed = 0
+        elif self.currentSpeed > self.maximumSpeed:
+            self.currentSpeed = self.maximumSpeed
+            
+    def drive(self, time):
+        self.travelledDistance = self.travelledDistance + self.currentSpeed * time
+        
+class ElectricCar(Car):
+    def __init__(self, registrationNumber, maximumSpeed, batteryCapacity):
+        super().__init__(registrationNumber, maximumSpeed)
+        self.batteryCapacity = batteryCapacity
+        
+class GasolineCar(Car):
+    def __init__(self, registrationNumber, maximumSpeed, tankVolume):
+        super().__init__(registrationNumber, maximumSpeed)
+        self.tankVolume = tankVolume
+        
+electricCar = ElectricCar('ABC-15', 180, 52.5)
+gasolineCar = GasolineCar('ACD-123', 165, 32.3)
+
+for i in range(3):
+    electricCar.accelerate(random.randint(-10,15))
+    gasolineCar.accelerate(random.randint(-10,15))
+    electricCar.drive(1)
+    gasolineCar.drive(1)
+    print(f"The electric car runs at {electricCar.currentSpeed}km/h. It has run {electricCar.travelledDistance}km.")
+    print(f"The gasoline car runs at {gasolineCar.currentSpeed}km/h. It has run {gasolineCar.travelledDistance}km.")
+        
